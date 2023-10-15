f535cc460e4340ed8d0e8521d9ec0c048c665789
ariftoteles <arif.santoso4@bithealth.com>
2023-10-15 20:23:57 +0700

    fixed bug

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const add = (a, b) => {
   return parseInt(a) + parseInt(b);
 };
 
-const sub = (a, b) => a + b;
-const mul = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
 module.exports = {
